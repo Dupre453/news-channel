038df63a379cbb0fe1e0944e3c3eb98aa842affd
Anna Chaplygina <dupreseraphine@gmail.com>
2024-04-10 16:16:15 +0300

    new styles

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import './../scss/pages/Home.scss'
 import PostBlock from "../components/post-block";
 import SideMenu from "../components/SideMenu";
+import {Avatar, IconButton} from "@mui/material";
+import PersonAdd from '@mui/icons-material/PersonAddAlt';
+import NewPost from "../components/new-post";
 const Home = () => {
 
     return (
@@ -13,6 +16,7 @@ const Home = () => {
             </section>
             <section className='news-feed'>
                 <h3>Главная</h3>
+                <NewPost/>
                 <PostBlock/>
                 <PostBlock/>
                 <PostBlock/>
@@ -23,6 +27,46 @@ const Home = () => {
             <section className='banners'>
                 <div>
                     <input className='search' type="text" placeholder='Поиск новостей...'/>
+                </div>
+                <div className='current-block'>
+                  <div className='current-block__title'>Актуальные темы</div>
+                    <ul>
+                        <li>
+                            <span className='title-post'>Белка-летяга</span>
+                            <div className='current-block__post'>постов:<span>13278</span></div>
+                        </li>
+                        <li>
+                            <span className='title-post'>#Билли</span>
+                            <div className='current-block__post'>постов:<span>12208</span></div>
+                        </li>
+                        <li>
+                            <span className='title-post'>Россия</span>
+                            <div className='current-block__post'>постов:<span>10976</span></div>
+                        </li>
+                    </ul>
+                </div>
+
+                <div className='read-block'>
+                    <div className='read-block__title'>Кого читать</div>
+                    <ul>
+                        <li>
+                            <div className='post-block__header'>
+                                <div className='post-block__avatar'>
+                                    <Avatar alt="Аватар пользователся"
+                                            src="https://images.unsplash.com/photo-1586351012965-861624544334?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"/>
+                                </div>
+                                <div className='post-block__name'>
+                                    <b>luv u </b>
+                                    <span> @apdasd</span>
+                                </div>
+                                <div>
+                                    <IconButton style={{color: "#c5c91f"}}>
+                                        <PersonAdd/>
+                                    </IconButton>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
                 </div>
             </section>
         </div>
